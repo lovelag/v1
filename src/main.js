24f766660fddd5b9211cec5832a88de8e328ad4c
jbernlove@gmail.com <jbernlove@gmail.com>
2020-02-16 20:03:39 +0800

    first commit3

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
+import "@babel/polyfill";
+import "mutationobserver-shim";
 import Vue from "vue";
+import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import jQuery from 'jquery';
 
 Vue.config.productionTip = false;
+
+window.$ = window.jQuery = jQuery;
+Vue.component('Navigation', require('./components/Navigation.vue').default);
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+   <b-container class="bv-example-row">
+  <b-row>
+    <b-col>1 of 3</b-col>
+    <b-col>2 of 3</b-col>
+    <b-col>3 of 3</b-col>
+  </b-row>
+</b-container>
   </div>
 </template>
